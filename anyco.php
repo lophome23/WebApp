d945a2f802ade11921353c0d9b5c8d02af82f919
lophome23 <prosperlozada@gmail.com>
2017-02-08 12:59:12 +0800

    Update anyco.php

--- anyco.php
+++ anyco.php
@@ -3,23 +3,25 @@
 	//ui_print_header('Derpartment');
 	//ui_print_footer(date('Y-m-d H: i: s'));
 	$conn = oci_connect('hr','hr','//localhost/XE');
-	ui_printer_header('Departmanets')
+	ui_print_header('Departments');
 	do_query($conn,'SELECT * FROM DEPARTMENTS');
 	ui_print_footer(date('Y-m-d H: i: s'));
 
 	function do_query($conn,$query){
 		$stid = oci_parse($conn,$query);
-		$r = oic_execute($stid,OCI_DEFAULT);
+		$r = oci_execute($stid,OCI_DEFAULT);
+
 		print'<table border ="1">';
 		while($row = oci_fetch_array($stid,OCI_RETURN_NULLS)){
 				print'<tr>';
 					foreach($row as $item){
-						print'<td>'
+						print'<td>'.
 							($item ? htmlentities($item):'&nbsp;').'</td>';
 					}
-					print'</td>';
+					print'</tr>';
 		}
-		print'</td>';
+		print'</table>';
 	}
+	
 ?>
 
